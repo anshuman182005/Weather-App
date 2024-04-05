737294ccb13296fb4994d9bd3f1754ec7f9fad24
anshuman <anshumanpandey182005@gmail.com>
2024-04-06 00:48:39 +0530

    added arrows

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Weather App Project
 This is a weather web app which is created using html,css,jquery,bootstrap and javascript.
 
-[Live Demo](https://ninja23482828484848.github.io/Weather-App-Web-Development/ "Weather App")
+[Live Demo](https://anshuman182005.github.io/Weather-App/ "Weather App")
 # Hope you like it
--- script.js
+++ script.js
@@ -16,18 +16,66 @@ function getWeather() {
             $("#wind-speed").html(response.wind_speed + " km/h");
             $("#cloud-pct").html(response.cloud_pct + " mm");
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err);
+            getWeather2();
+        });
 }
 
-// function getWeather() {
-//     let city = $("#location-data").val();
-//     fetch('http://api.weatherapi.com/v1/current.json?key=8956c7019a614fec8b9175258231302&q='+city+'&aqi=yes')
-//         .then(response => response.json())
-//         .then(data => {
-//             $("#temp").html(data.current.temp_c + "&#176;C");
-//             $("#humidity").html(data.current.humidity + " %");
-//             $("#wind-speed").html(data.current.wind_kph + " km/h");
-//             $("#cloud-pct").html(data.current.precip_mm + " mm");
-//         })
-//         .catch(error => console.error(error));
-// }
+function getWeather2() {
+    let city = $("#location-data").val();
+    fetch('http://api.weatherapi.com/v1/current.json?key=8956c7019a614fec8b9175258231302&q='+city+'&aqi=yes')
+        .then(response => response.json())
+        .then(data => {
+            $("#temp").html(data.current.temp_c + "&#176;C");
+            $("#humidity").html(data.current.humidity + " %");
+            $("#wind-speed").html(data.current.wind_kph + " km/h");
+            $("#cloud-pct").html(data.current.precip_mm + " mm");
+        })
+        .catch(error => console.error(error));
+}
+
+var slideIndex = 1;
+function rightSlide() {
+    slideIndex++;
+    if (slideIndex > 3) {
+        slideIndex = 1;
+    }
+    if (slideIndex == 1) {
+        $("#wind-card").removeClass("d-none");
+        $("#cloud-card").addClass("d-none");
+        $("#humidity-card").addClass("d-none");
+    }
+    else if (slideIndex == 2) {
+        $("#wind-card").addClass("d-none");
+        $("#cloud-card").removeClass("d-none");
+        $("#humidity-card").addClass("d-none");
+    }
+    else {
+        $("#wind-card").addClass("d-none");
+        $("#cloud-card").addClass("d-none");
+        $("#humidity-card").removeClass("d-none");
+    }
+}
+
+function leftSlide() {
+    slideIndex--;
+    if (slideIndex < 1) {
+        slideIndex = 3;
+    }
+    if (slideIndex == 1) {
+        $("#wind-card").removeClass("d-none");
+        $("#cloud-card").addClass("d-none");
+        $("#humidity-card").addClass("d-none");
+    }
+    else if (slideIndex == 2) {
+        $("#wind-card").addClass("d-none");
+        $("#cloud-card").removeClass("d-none");
+        $("#humidity-card").addClass("d-none");
+    }
+    else {
+        $("#wind-card").addClass("d-none");
+        $("#cloud-card").addClass("d-none");
+        $("#humidity-card").removeClass("d-none");
+    }
+}
